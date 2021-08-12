0cf726600554f2d057cb58895e6813791ce1b0ee
maratzahidulin <maratzahidulin@gmail.com>
2021-08-12 18:17:06 +0300

    Empty project.

---
+++ Assets/Experimental/Experimental.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class Experimental : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaSpeed = speed * Time.deltaTime;
+        transform.Rotate(deltaSpeed,deltaSpeed,deltaSpeed);
+    }
+}
